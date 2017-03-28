25fee60dd0ad378f8b0289d8046cabf95c263856
TNeuhaus <TNeuhaus@00-46344.pusd.dom>
2017-03-28 16:51:07 -0700

    Added another print statement

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello");
-		System.out.println("Hello goodbye");		
+		System.out.println("Hello goodbye");
+		System.out.println("Another change");
 	}
 }
